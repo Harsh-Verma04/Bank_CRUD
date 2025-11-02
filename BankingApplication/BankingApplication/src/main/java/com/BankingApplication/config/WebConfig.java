d5662235025c4373d269f6698f0ac3babb0b9d0a
Harsh Harsh <canwemeetharsh@gmail.com>
2025-11-02 17:20:17 +0530

    vercel.

--- BankingApplication/BankingApplication/src/main/java/com/BankingApplication/config/WebConfig.java
+++ BankingApplication/BankingApplication/src/main/java/com/BankingApplication/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig {
                         // Add your Angular app's URLs here
                         .allowedOrigins(
                             "http://localhost:4200", // For local development
-                            "https://bank-crud-gray.vercel.app" // Your deployed Angular app URL
+                            "https://bank-crud-livid.vercel.app/" // Your deployed Angular app URL
                         ) 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
