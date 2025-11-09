4af8f0ada17a72cc3d6c7b6b96bb9d22d1eeaf10
Harsh <86431354+Harsh-Verma04@users.noreply.github.com>
2025-11-09 17:34:36 +0530

    Update WebConfig.java

--- BankingApplication/BankingApplication/src/main/java/com/BankingApplication/config/WebConfig.java
+++ BankingApplication/BankingApplication/src/main/java/com/BankingApplication/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
                         
                         // Add your Angular app's URLs here
                         .allowedOrigins(
-                            "http://localhost:4200", // For local development
+                           "http://localhost:5173",// For local development
                             "https://bank-crud-livid.vercel.app" // Your deployed Angular app URL
                         ) 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
